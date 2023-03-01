afe0f6e2184bea1cb5804db03d70e40a0f03c24f
Sebazt <juan.londono29@udea.edu.co>
2023-03-01 12:51:06 -0500

    :sparkles: add components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <h1>Hi</h1>
+  <Suspense>  <!-- recibe dos slot -->
+    <template #default> <!-- pantalla ppal -->
+      <Home/>
+    </template>
+    <template #fallback>  <!-- Carga  -->
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
 
 <script>
+import SplashScreen from '@/components/SplashScreen.vue'
+import { defineAsyncComponent } from 'vue'
 
 
 export default {
   name: 'App',
   components: {
-
+    SplashScreen,
+    Home: defineAsyncComponent(()=>
+      new Promise((res)=>{
+        setTimeout(() =>{
+          res(import("./components/Home.vue"))
+        }, 2500)
+      })
+    )
   }
 }
 </script>
 
 <style>
-
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+
+    <template #resume>
+      <Resume></Resume>
+    </template>
+
+    <template #movements>
+      <Movements></Movements>
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue'
+import Header from './Header.vue'
+import Resume from './Resume.vue'
+import Movements from './Movements.vue'
+
+export default {
+  components:{
+    Layout,
+    Header,
+    Resume,
+    Movements
+
+  }
+}
+</script>
